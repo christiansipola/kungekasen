f1162d2d367f73e15d3727f28f21677cd4c266f5
Christian Sipola <christian@kaustik.com>
2014-03-12 20:33:46 +0100

    add silex, twig and phpstorm project

---
+++ web/index.php
@@ -0,0 +1,16 @@
+<?php
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+$app['debug'] = true;
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views',
+));
+
+$app->get('/', function() use($app) {
+    return $app['twig']->render('hello.html.twig', array());
+});
+
+$app->run(); 
